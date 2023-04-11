b9303536f8883c963926f7d0e17749fbe57a8f1a
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-04-11 21:21:56 +0530

    :hammer: Write a JavaScript code to get positive numbers from array of integers.

--- javascript-beginner/even-and-odd-number/script.js
+++ javascript-beginner/even-and-odd-number/script.js
@@ -11,3 +11,11 @@ console.log(getEvenNumbers);
 const getOddNumbers = numbers.filter((integer) => integer % 2 !== 0);
 
 console.log(getOddNumbers);
+
+/* Write a JavaScript code to get positive numbers from array of integers */
+
+const integers = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+
+const getPositiveIntegers = integers.filter((integer) => integer > 0);
+
+console.log(getPositiveIntegers);
