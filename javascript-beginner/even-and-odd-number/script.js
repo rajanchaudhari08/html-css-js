1dc144a73e98cba349b5ae0378dcbe3d6d2b0fa4
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-04-11 21:12:02 +0530

    :hammer: Write a JavaScript code to get odd numbers from array of integers.

--- javascript-beginner/even-and-odd-number/script.js
+++ javascript-beginner/even-and-odd-number/script.js
@@ -5,3 +5,9 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const getEvenNumbers = numbers.filter((integer) => integer % 2 === 0);
 
 console.log(getEvenNumbers);
+
+/* Write a JavaScript code to get odd numbers from array of integers */
+
+const getOddNumbers = numbers.filter((integer) => integer % 2 !== 0);
+
+console.log(getOddNumbers);
