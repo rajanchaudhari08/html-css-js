e4e163e383cc3d652ececca1c3735d5344c7f696
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-04-11 21:28:09 +0530

    :hammer: Write a JavaScript code to get negative numbers from array of integers.

--- javascript-beginner/even-and-odd-number/script.js
+++ javascript-beginner/even-and-odd-number/script.js
@@ -19,3 +19,9 @@ const integers = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
 const getPositiveIntegers = integers.filter((integer) => integer > 0);
 
 console.log(getPositiveIntegers);
+
+/* Write a JavaScript code to get negative numbers from array of integers */
+
+const getNegativeIntegers = integers.filter((integer) => integer < 0);
+
+console.log(getNegativeIntegers);
