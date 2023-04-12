3829c696394bf3defa797a7e546fc417aefcc07f
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-04-12 22:48:03 +0530

    :hammer: Write a JavaScript code to find the length of string.

---
+++ javascript-beginner/string/string-length/script.js
@@ -0,0 +1,17 @@
+/* Write a JavaScript code to find the length of string */
+
+console.log("Frontend Web UI Developer".length);
+
+/* ----- */
+
+let username = "rajanchaudhari08";
+
+console.log(username.length);
+
+/* ----- */
+
+let country = "India";
+
+let get__StringLength = country.length;
+
+console.log(get__StringLength);
